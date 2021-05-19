d917ae85b6d84aaae1f988662258acd3f6cf66c1
drewnutt <anm329@pitt.edu>
2021-05-19 16:32:12 -0400

    fix issue with multiple things in state_dict

--- moco/default2018_single_model.py
+++ moco/default2018_single_model.py
@@ -16,25 +16,16 @@ class Net(nn.Module):
         self.func = F.relu
 
         self.avgpool1 = nn.AvgPool3d(2,stride=2)
-        self.add_module('avgpool_0', self.avgpool1)
         self.conv1 = nn.Conv3d(nchannels,out_channels=32,padding=1,kernel_size=3,stride=1) 
-        self.add_module('unit1_conv',self.conv1)
         self.conv2 = nn.Conv3d(32,out_channels=32,padding=0,kernel_size=1,stride=1) 
-        self.add_module('unit2_conv',self.conv2)
         self.avgpool2 = nn.AvgPool3d(2,stride=2)
-        self.add_module('avgpool_1', self.avgpool2)
         self.conv3 = nn.Conv3d(32,out_channels=64,padding=1,kernel_size=3,stride=1) 
-        self.add_module('unit3_conv',self.conv3)
         self.conv4 = nn.Conv3d(64,out_channels=64,padding=0,kernel_size=1,stride=1) 
-        self.add_module('unit4_conv',self.conv4)
         self.avgpool3 = nn.AvgPool3d(2,stride=2)
-        self.add_module('avgpool_2', self.avgpool3)
         self.conv5 = nn.Conv3d(64,out_channels=128,padding=1,kernel_size=3,stride=1) 
-        self.add_module('unit5_conv',self.conv5)
         div = 2*2*2
         last_size = int(dims[1]//div * dims[2]//div * dims[3]//div * 128)
         self.flattener = View((-1,last_size))
-        self.add_module('flatten',self.flattener)
         self.fc = nn.Linear(last_size,num_classes)
 
     def forward(self, x): 
